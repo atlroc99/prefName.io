159aaeaebe1a2a0edcbf03e1d9abbd3a1e53aa88
atlroc99 <jeffry.zaman>
2018-05-03 11:46:56 -0400

    fixed misplled

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ inputs[0].addEventListener("keyup",function (event) {
 
 inputs[1].addEventListener("click", function(){
 	console.log('Button cliked..');
-	alert("not impleted yet..booya :/")
+	alert("not implemented yet..booya :/")
 });
 
 
